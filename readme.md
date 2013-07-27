2a26664d8cdd82089d7871073736903c43deefe6
iamthes <s@home.local>
2013-07-28 01:08:24 +0400

    Removed done things, debug code

--- readme.md
+++ readme.md
@@ -7,8 +7,5 @@ TODO
 - open edit/add dialog near parent frame
 - use: java network launching protocol (jnlp) http://ru.wikipedia.org/wiki/Java_Web_Start
 - default view in descending mode
-- check for errors while adding/editing
-- use rowinserted in fireTableDataChanged
-- hide column
 - attach SwingX AutoCompleteDecorator to category field
 - open picture dialog open open in near directory
--- src/phonebook/ui/CategoryComboBoxModel.java
+++ src/phonebook/ui/CategoryComboBoxModel.java
@@ -44,27 +44,4 @@ public class CategoryComboBoxModel extends DefaultComboBoxModel {
 		}
 		return (Category) anItem;
 	}
-
-//	@Override
-//	public void setSelectedItem(Object anItem) {
-//		System.out.println("CategoryComboBoxModel.setSelectedItem " + anItem);
-//		if (anItem == null) {
-//			anItem = 0;
-//		}
-//		if (anItem instanceof Category) {
-//			setSelectedItem((Category) anItem);
-//		} else if (anItem instanceof Integer) {
-//			Category category = categoryModel.getId((Integer) anItem);
-//			setSelectedItem(category);
-//		} else if (anItem instanceof String) {
-//			Category category = categoryModel.getByName(anItem.toString());
-//			if (category == null && anItem.toString().length() > 0) {
-//				category = categoryModel.create(anItem.toString());
-//				dataList = null;
-//			}
-//			setSelectedItem(category);
-//		} else {
-//			throw new InvalidParameterException(String.format("Unexpected %s (%s).", anItem.getClass(), anItem));
-//		}
-//	}
 }
